28a3349fedcfaa7a595b60cd587aa55f50a8a931
srabondhr <srabandhr2@gmail.com>
2025-02-11 17:50:20 +0600

    Add files via upload

---
+++ Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,28 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
+#import <firebase_core/FLTFirebaseCorePlugin.h>
+#else
+@import firebase_core;
+#endif
+
+#if __has_include(<flutter_app_update/FlutterAppUpdatePlugin.h>)
+#import <flutter_app_update/FlutterAppUpdatePlugin.h>
+#else
+@import flutter_app_update;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTFirebaseCorePlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTFirebaseCorePlugin"]];
+  [FlutterAppUpdatePlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterAppUpdatePlugin"]];
+}
+
+@end
